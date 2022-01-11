5d4a0f298103679d1af1ccbad47baa5a65fbfb61
Francososa <franky.sosa@gmail.com>
2022-01-11 11:32:03 -0600

    add tests to interact with solana program

--- programs/francoin/src/lib.rs
+++ programs/francoin/src/lib.rs
@@ -1,7 +1,7 @@
 use anchor_lang::prelude::*;
 use anchor_spl::token::{self, Burn, MintTo, SetAuthority, Transfer};
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("3PXoLHwbQfpwjRB8evrpV8sAqs5LgTvu3HS4SHU2uBN7");
 
 #[program]
 pub mod francoin {
